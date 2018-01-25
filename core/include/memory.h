intel/haxm

commit ab4c61ee2dad855b299717924b23fc823be189d4

core: Enable reference counting for RAM blocks
* Implement ramblock_ref and ramblock_deref interfaces.
* Remove RAM block automatically when reference count hits zero.
* Invoke above interfaces from memslot module.

Currently, QEMU only creates RAM blocks at guest initialization
time, and there's no need to free these RAM blocks before the guest
terminates. But if we allow RAM blocks to be created dynamically
during guest runtime, e.g., to map temporary OpenGL buffers backed
by host GPU memory directly into the guest, we should make sure
each of these temporary RAM blocks gets freed as soon as all
mappings to it from guest physical address space are removed;
otherwise, we'll be leaking host memory over time. We know a RAM
block can be freed when no memslot points to it, which is why we
want to add reference counting logic.

core/include/memory.h
@@ -53,9 +53,10 @@ typedef struct hax_ramblock {
     // One bit per chunk indicating whether the chunk has been (or is being)
     // allocated/pinned or not
     uint8 *chunks_bitmap;
+    // Reference count of this object
+    int ref_count;
     // Turns this object into a list node
     hax_list_node entry;
-    // TODO: refcount?
 } hax_ramblock;
 
 typedef struct hax_memslot {
@@ -80,6 +81,8 @@ typedef struct hax_memslot {
 #define HAX_MEMSLOT_INVALID  0x80
 
 typedef struct hax_gpa_space {
+    // TODO: Add a lock to prevent concurrent accesses to |ramblock_list| and
+    // |memslot_list|
     hax_list_head ramblock_list;
     hax_list_head memslot_list;
     hax_list_head listener_list;
@@ -162,6 +165,20 @@ int ramblock_add(hax_list_head *list, uint64 base_uva, uint64 size,
 //    was not successful.
 hax_chunk * ramblock_get_chunk(hax_ramblock *block, uint64 uva_offset,
                                bool alloc);
+
+// Increments the reference count of an existing RAM block. The reference count
+// of a new RAM block created by ramblock_add() is initialized to 0. Whenever a
+// new reference to a RAM block is made, this function must be called.
+// |block|: A pointer to |hax_ramblock| being referenced.
+void ramblock_ref(hax_ramblock *block);
+
+// Decrements the reference count of the specified RAM block. Whenever a
+// reference to a RAM block is removed, this function must be called. If the
+// resulting reference count hits zero, removes the RAM block from the list it
+// belongs to, and frees the RAM block along with all the resources allocated
+// for it.
+// |block|: A pointer to |hax_ramblock| being dereferenced.
+void ramblock_deref(hax_ramblock *block);
 
 // Initializes |hax_memslot|-related data structures in the given
 // |hax_gpa_space|.

commit 5a4d57896e5478a2398004f729dcc164a5ebe79d

Reland "core: Enable reference counting"
* Revert commit 17d656594e1097cc9ec8cac92a412de0add4502c.
* Improve the mechanism of reference counting of RAM blocks.

The previous patch ab4c61e introduced a regression, which resulted
in QEMU failing to map into some RAM blocks when launching some
guests, such as Debian and ChromeOS. The root cause was that patch
implemented an algorithm to destroy a RAM block once its reference
count hits zero for memory reclaim, but the said guests then tried
to create another mapping into the same RAM block, and failed.
This patch changes the algorithm by only freeing chunks and reset
chunks bitmap related to a RAM block when reference count hits zero,
instead of freeing the whole RAM block.

core/include/memory.h
@@ -53,9 +53,10 @@ typedef struct hax_ramblock {
     // One bit per chunk indicating whether the chunk has been (or is being)
     // allocated/pinned or not
     uint8 *chunks_bitmap;
+    // Reference count of this object
+    int ref_count;
     // Turns this object into a list node
     hax_list_node entry;
-    // TODO: refcount?
 } hax_ramblock;
 
 typedef struct hax_memslot {
@@ -80,6 +81,8 @@ typedef struct hax_memslot {
 #define HAX_MEMSLOT_INVALID  0x80
 
 typedef struct hax_gpa_space {
+    // TODO: Add a lock to prevent concurrent accesses to |ramblock_list| and
+    // |memslot_list|
     hax_list_head ramblock_list;
     hax_list_head memslot_list;
     hax_list_head listener_list;
@@ -162,6 +165,20 @@ int ramblock_add(hax_list_head *list, uint64 base_uva, uint64 size,
 //    was not successful.
 hax_chunk * ramblock_get_chunk(hax_ramblock *block, uint64 uva_offset,
                                bool alloc);
+
+// Increments the reference count of an existing RAM block. The reference count
+// of a new RAM block created by ramblock_add() is initialized to 0. Whenever a
+// new reference to a RAM block is made, this function must be called.
+// |block|: A pointer to |hax_ramblock| being referenced.
+void ramblock_ref(hax_ramblock *block);
+
+// Decrements the reference count of the specified RAM block. Whenever a
+// reference to a RAM block is removed, this function must be called. If the
+// resulting reference count hits zero, removes the RAM block from the list it
+// belongs to, and frees the RAM block along with all the resources allocated
+// for it.
+// |block|: A pointer to |hax_ramblock| being dereferenced.
+void ramblock_deref(hax_ramblock *block);
 
 // Initializes |hax_memslot|-related data structures in the given
 // |hax_gpa_space|.

core/memslot.c
@@ -35,7 +35,7 @@
 #define MEMSLOT_PROCESSING 0x01
 #define MEMSLOT_TO_INSERT  0x02
 
-#define MEMSLOT_ALLOC(m) if (((m) = memslot_alloc(m)) == NULL) return -ENOMEM
+#define MEMSLOT_DUP(m) if (((m) = memslot_dup(m)) == NULL) return -ENOMEM
 #define SAFE_CALL(f) if ((f) != NULL) (f)
 
 enum callback {
@@ -62,13 +62,13 @@ typedef struct memslot_mapping {
     uint8 new_flags;
 } memslot_mapping;
 
-static hax_memslot * memslot_alloc(hax_memslot *dest);
-static void memslot_free(void **memslot, uint32 size);
-static void memslot_copy(hax_memslot *dest, hax_memslot *src);
+static void memslot_init(hax_memslot *dest, hax_memslot *src);
+static hax_memslot * memslot_dup(hax_memslot *slot);
 static void memslot_insert_before(hax_memslot *dest, hax_memslot *src);
 static void memslot_insert_after(hax_memslot *dest, hax_memslot *src);
 static void memslot_insert_head(hax_memslot *dest, hax_gpa_space *gpa_space);
 static void memslot_delete(hax_memslot *dest);
+static void memslot_move(hax_memslot *dest, hax_memslot *src);
 static void memslot_union(hax_memslot *dest, hax_memslot *src);
 static void memslot_overlap_front(hax_memslot *dest, hax_memslot *src);
 static void memslot_overlap_rear(hax_memslot *dest, hax_memslot *src);
@@ -96,7 +96,7 @@ static void mapping_calc_change(memslot_mapping *mapping, hax_memslot *src,
                                 bool is_valid, bool is_terminal,
                                 bool is_changed, memslot_mapping *hole,
                                 memslot_mapping *slot);
-static void mapping_enqueue(hax_list_head *memslot_list, hax_memslot *dest);
+static int mapping_enqueue(hax_list_head *memslot_list, hax_memslot *dest);
 static void mapping_intersect(memslot_mapping *dest, memslot_mapping *src);
 static bool mapping_is_changed(hax_memslot *dest, hax_memslot *src,
                                bool is_valid, bool is_terminal);
@@ -147,7 +147,7 @@ void memslot_dump_list(hax_gpa_space *gpa_space)
     hax_info("memslot dump begins:\n");
     hax_list_entry_for_each(memslot, &gpa_space->memslot_list, hax_memslot,
                             entry) {
-        hax_info("memory slot [%d]: base_gfn = 0x%016llx, npages = 0x%llx, "
+        hax_info("memslot [%d]: base_gfn = 0x%016llx, npages = 0x%llx, "
                  "uva = 0x%016llx, flags = 0x%02x "
                  "(block_base_uva = 0x%016llx, offset_within_block = 0x%llx)\n",
                  i++, memslot->base_gfn, memslot->npages,
@@ -200,13 +200,13 @@ int memslot_set_mapping(hax_gpa_space *gpa_space, uint64 start_gfn,
 
     if (hax_list_empty(&gpa_space->memslot_list)) {
         if (is_valid) {
-            MEMSLOT_ALLOC(dest);
+            MEMSLOT_DUP(dest);
             memslot_insert_head(dest, gpa_space);
 
             mapping_broadcast(&gpa_space->listener_list, &mapping, dest, NULL);
         }
 
-        return 0;
+        goto out;
     }
 
     hax_init_list_head(&memslot_list);
@@ -219,7 +219,7 @@ int memslot_set_mapping(hax_gpa_space *gpa_space, uint64 start_gfn,
                     continue;
 
                 if (is_valid) {
-                    MEMSLOT_ALLOC(dest);
+                    MEMSLOT_DUP(dest);
                     memslot_insert_after(dest, src);
                 }
                 break;
@@ -237,7 +237,7 @@ int memslot_set_mapping(hax_gpa_space *gpa_space, uint64 start_gfn,
                     : MAPPING_INVALID;
             ret = memslot_process_start[route](dest, src, &state);
             if (ret != 0)
-                return ret;
+                goto out;
 
             is_found = true;
         } else {
@@ -257,7 +257,7 @@ int memslot_set_mapping(hax_gpa_space *gpa_space, uint64 start_gfn,
                     : MAPPING_INVALID;
             ret = memslot_process_end[route](dest, src, &state);
             if (ret != 0)
-                return ret;
+                goto out;
 
             state = 0;
         }
@@ -267,14 +267,23 @@ int memslot_set_mapping(hax_gpa_space *gpa_space, uint64 start_gfn,
     }
 
     if (state & MEMSLOT_TO_INSERT) {
-        MEMSLOT_ALLOC(dest);
+        MEMSLOT_DUP(dest);
         memslot_insert_before(dest, src);
     }
 
     mapping_broadcast(&gpa_space->listener_list, &mapping, dest,
                       &memslot_list);
 
-    return 0;
+out:
+    // ramblock_find() was invoked previously in this function and returned a
+    // pointer to an existing |hax_ramblock|, whose refcount was incremented by
+    // ramblock_find(). Now that the pointer (local variable |block|) is about
+    // to go out of scope, ramblock_deref() must be invoked here to keep the
+    // refcount accurate.
+    if (block != NULL) {
+        ramblock_deref(block);
+    }
+    return ret;
 }
 
 hax_memslot * memslot_find(hax_gpa_space *gpa_space, uint64 gfn)
@@ -296,41 +305,27 @@ hax_memslot * memslot_find(hax_gpa_space *gpa_space, uint64 gfn)
     return NULL;
 }
 
-static inline hax_memslot * memslot_alloc(hax_memslot *memslot)
+static void memslot_init(hax_memslot *dest, hax_memslot *src)
 {
-    hax_memslot *ptr = NULL;
+    *dest = *src;
+    ramblock_ref(dest->block);
+    hax_init_list_head(&dest->entry);
+}
 
-    ptr = (hax_memslot *)hax_vmalloc(sizeof(hax_memslot), HAX_MEM_NONPAGE);
+static inline hax_memslot * memslot_dup(hax_memslot *slot)
+{
+    hax_memslot *new_slot = NULL;
 
-    if (ptr == NULL)
+    new_slot = (hax_memslot *)hax_vmalloc(sizeof(hax_memslot), HAX_MEM_NONPAGE);
+
+    if (new_slot == NULL) {
+        hax_error("%s: Failed to allocate memslot\n", __func__);
         return NULL;
+    }
 
-    ptr->base_gfn            = memslot->base_gfn;
-    ptr->npages              = memslot->npages;
-    ptr->block               = memslot->block;
-    ptr->offset_within_block = memslot->offset_within_block;
-    ptr->flags               = memslot->flags;
-    hax_init_list_head(&ptr->entry);
+    memslot_init(new_slot, slot);
 
-    return ptr;
-}
-
-static inline void memslot_free(void **va, uint32 size)
-{
-    if ((va == NULL) || (*va == NULL))
-        return;
-
-    hax_vfree(*va, size);
-    *va = NULL;
-}
-
-static inline void memslot_copy(hax_memslot *dest, hax_memslot *src)
-{
-    dest->base_gfn            = src->base_gfn;
-    dest->npages              = src->npages;
-    dest->block               = src->block;
-    dest->offset_within_block = src->offset_within_block;
-    dest->flags               = src->flags;
+    return new_slot;
 }
 
 static inline void memslot_insert_before(hax_memslot *dest, hax_memslot *src)
@@ -352,7 +347,14 @@ static inline void memslot_insert_head(hax_memslot *dest,
 static inline void memslot_delete(hax_memslot *dest)
 {
     hax_list_del(&dest->entry);
-    memslot_free((void **)&dest, sizeof(hax_memslot));
+    ramblock_deref(dest->block);
+    hax_vfree(dest, sizeof(hax_memslot));
+}
+
+static inline void memslot_move(hax_memslot *dest, hax_memslot *src)
+{
+    ramblock_deref(dest->block);
+    memslot_init(dest, src);
 }
 
 static inline void memslot_union(hax_memslot *dest, hax_memslot *src)
@@ -381,21 +383,24 @@ static inline void memslot_overlap_rear(hax_memslot *dest, hax_memslot *src)
 static inline hax_memslot * memslot_append_rest(hax_memslot *dest,
                                                 hax_memslot *src)
 {
-    hax_memslot *rest = NULL;
+    hax_memslot *rest, slot;
 
     rest = (hax_memslot *)hax_vmalloc(sizeof(hax_memslot), HAX_MEM_NONPAGE);
 
-    if (rest == NULL)
+    if (rest == NULL) {
+        hax_error("%s: Failed to allocate memslot\n", __func__);
         return NULL;
+    }
 
-    rest->base_gfn            = dest->base_gfn + dest->npages;
-    rest->npages              = src->base_gfn + src->npages - dest->base_gfn
-                                - dest->npages;
-    rest->block               = src->block;
-    rest->offset_within_block = src->offset_within_block + ((dest->base_gfn
-                                + dest->npages - src->base_gfn) << PG_ORDER_4K);
-    rest->flags               = src->flags;
-    hax_init_list_head(&rest->entry);
+    slot.base_gfn            = dest->base_gfn + dest->npages;
+    slot.npages              = src->base_gfn + src->npages - dest->base_gfn
+                               - dest->npages;
+    slot.block               = src->block;
+    slot.offset_within_block = src->offset_within_block + ((dest->base_gfn
+                               + dest->npages - src->base_gfn) << PG_ORDER_4K);
+    slot.flags               = src->flags;
+
+    memslot_init(rest, &slot);
 
     return rest;
 }
@@ -466,13 +471,13 @@ static int memslot_process_start_diff_type(hax_memslot *dest, hax_memslot *src,
 {
     if (dest->base_gfn + dest->npages <= src->base_gfn) {
         // (1)(2)
-        MEMSLOT_ALLOC(dest);
+        MEMSLOT_DUP(dest);
         memslot_insert_before(dest, src);
     } else if (dest->base_gfn <= src->base_gfn) {
         // (3)(4)(5)(6)(7)(8)
         if (dest->base_gfn + dest->npages < src->base_gfn + src->npages) {
             // (3)(6)
-            MEMSLOT_ALLOC(dest);
+            MEMSLOT_DUP(dest);
             memslot_insert_before(dest, src);
             memslot_overlap_front(dest, src);
         } else {
@@ -490,7 +495,7 @@ static int memslot_process_start_diff_type(hax_memslot *dest, hax_memslot *src,
             // (9)(10)(11)
             if (dest->base_gfn + dest->npages < src->base_gfn + src->npages) {
                 // (9)
-                MEMSLOT_ALLOC(dest);
+                MEMSLOT_DUP(dest);
                 memslot_insert_after(dest, src);
                 memslot_insert_after(memslot_append_rest(dest, src), dest);
             }
@@ -506,7 +511,7 @@ static int memslot_process_start_same_type(hax_memslot *dest, hax_memslot *src,
 {
     if (dest->base_gfn + dest->npages < src->base_gfn) {
         // (1)
-        MEMSLOT_ALLOC(dest);
+        MEMSLOT_DUP(dest);
         memslot_insert_before(dest, src);
         return 0;
     }
@@ -583,13 +588,13 @@ static int memslot_process_end_diff_type(hax_memslot *dest, hax_memslot *src,
             memslot_overlap_front(dest, src);
         }
         if (*state & MEMSLOT_TO_INSERT) {
-            MEMSLOT_ALLOC(dest);
+            MEMSLOT_DUP(dest);
             memslot_insert_before(dest, src);
         }
     } else {
         // [3]
         if (*state & MEMSLOT_TO_INSERT) {
-            memslot_copy(src, dest);
+            memslot_move(src, dest);
         } else {
             memslot_delete(src);
         }
@@ -729,21 +734,13 @@ static void mapping_calc_change(memslot_mapping *mapping, hax_memslot *src,
     }
 }
 
-static inline void mapping_enqueue(hax_list_head *memslot_list,
-                                   hax_memslot *dest)
+static inline int mapping_enqueue(hax_list_head *memslot_list,
+                                  hax_memslot *slot)
 {
-    hax_memslot *ptr = NULL;
+    MEMSLOT_DUP(slot);
+    hax_list_insert_before(&slot->entry, memslot_list);
 
-    if (dest == NULL)
-        return;
-
-    ptr = (hax_memslot *)hax_vmalloc(sizeof(hax_memslot), HAX_MEM_NONPAGE);
-
-    if (ptr == NULL)
-        return;
-
-    *ptr = *dest;
-    hax_list_insert_before(&ptr->entry, memslot_list);
+    return 0;
 }
 
 static inline void mapping_intersect(memslot_mapping *dest,

core/ramblock.c
@@ -93,20 +93,18 @@ static hax_ramblock * ramblock_alloc(uint64 base_uva, uint64 size)
     }
     memset(chunks_bitmap, 0, chunks_bitmap_size);
     block->chunks_bitmap = chunks_bitmap;
+    block->ref_count = 0;
+
     return block;
 }
 
-static void ramblock_free(hax_ramblock *block)
+static void ramblock_free_chunks(hax_ramblock *block, bool destroy)
 {
     hax_chunk **chunks;
     uint64 nchunks, chunks_bitmap_size, i;
     uint64 nbytes_used = 0;
 
-    if (!block) {
-        hax_warning("%s: block == NULL\n", __func__);
-        return;
-    }
-
+    assert(block != NULL);
     // Assuming block->chunks != NULL due to a previous ramblock_alloc() call
     chunks = block->chunks;
     nchunks = ramblock_count_chunks(block);
@@ -125,19 +123,25 @@ static void ramblock_free(hax_ramblock *block)
         }
         nbytes_used += chunk->size;
 
-        // Free the hax_chunk object
-        ret = chunk_free(chunk);
-        if (ret) {
-            hax_warning("%s: Failed to free chunk: i=%llu, base_uva=0x%llx,"
-                        " size=0x%llx, ret=%d\n", __func__, i, chunk->base_uva,
-                        chunk->size, ret);
-        }
-        chunks[i] = NULL;
+        // Three steps are needed to free a chunk:
+        //  1) Call chunk_free()
+        //  2) Clear chunks[i]
+        //  3) Clear bit i of chunks_bitmap
+        // But if they are carried out in this order, between 1) and 2), another
+        // thread may call ramblock_get_chunk() and get a bad pointer. So
+        // reverse the order to avoid that.
         if (hax_test_and_clear_bit((int) i, (uint64 *) block->chunks_bitmap)) {
             // Bit i of chunks_bitmap was already clear
             hax_warning("%s: chunks[%llu] existed but its bit in chunks_bitmap"
                         " was not set: size=0x%llx, block.size=0x%llx\n",
                         __func__, i, chunk->size, block->size);
+        }
+        chunks[i] = NULL;
+        ret = chunk_free(chunk);
+        if (ret) {
+            hax_warning("%s: Failed to free chunk: i=%llu, base_uva=0x%llx,"
+                        " size=0x%llx, ret=%d\n", __func__, i, chunk->base_uva,
+                        chunk->size, ret);
         }
     }
     // Double check that there is no bit set in chunks_bitmap
@@ -145,14 +149,30 @@ static void ramblock_free(hax_ramblock *block)
         if (block->chunks_bitmap[i]) {
             hax_warning("%s: chunks_bitmap[%llu]=0x%02x\n", __func__, i,
                         block->chunks_bitmap[i]);
+            block->chunks_bitmap[i] = 0;
         }
     }
+
+    hax_info("%s: All chunks freed: %lluKB total, %lluKB used\n", __func__,
+             block->size / 1024, nbytes_used / 1024);
+
+    if (!destroy)
+        return;
+
     // Free the chunks bitmap
     hax_vfree(block->chunks_bitmap, chunks_bitmap_size);
     // Free the chunks array
     hax_vfree(chunks, nchunks * sizeof(*chunks));
-    hax_info("%s: Freeing RAM block: %lluKB total, %lluKB used\n", __func__,
-             block->size / 1024, nbytes_used / 1024);
+}
+
+static void ramblock_free(hax_ramblock *block)
+{
+    if (!block) {
+        hax_warning("%s: block == NULL\n", __func__);
+        return;
+    }
+
+    ramblock_free_chunks(block, true);
     // Free the hax_ramblock object
     hax_vfree(block, sizeof(*block));
 }
@@ -181,8 +201,9 @@ void ramblock_free_list(hax_list_head *list)
     ramblock_info("ramblock_free_list\n");
     hax_list_entry_for_each_safe(ramblock, tmp, list, hax_ramblock, entry) {
         hax_list_del(&ramblock->entry);
-        ramblock_info("ramblock_free_list: freeing block: uva:0x%llx,"
-                      " size:0x%llx \n", ramblock->base_uva, ramblock->size);
+        ramblock_info("%s: Freeing RAM block: uva: 0x%llx, size: 0x%llx, "
+                      "ref_count: %d\n", __func__, ramblock->base_uva,
+                      ramblock->size, ramblock->ref_count);
         ramblock_free(ramblock);
     }
 }
@@ -194,8 +215,9 @@ void ramblock_dump_list(hax_list_head *list)
 
     ramblock_info("ramblock dump begin:\n");
     hax_list_entry_for_each(ramblock, list, hax_ramblock, entry) {
-        ramblock_info("block %d: base_uva 0x%llx, size 0x%llx\n",
-                      i++, ramblock->base_uva, ramblock->size);
+        ramblock_info("block %d (%p): base_uva 0x%llx, size 0x%llx, ref_count "
+                      "%d\n", i++, ramblock, ramblock->base_uva,
+                      ramblock->size, ramblock->ref_count);
     }
     ramblock_info("ramblock dump end!\n");
 }
@@ -210,8 +232,14 @@ hax_ramblock * ramblock_find(hax_list_head *list, uint64 uva,
         if (ramblock->base_uva > uva)
             break;
 
-        if (uva < ramblock->base_uva + ramblock->size)
+        if (uva < ramblock->base_uva + ramblock->size) {
+            hax_debug("%s: (%p): base_uva 0x%llx, size 0x%llx, ref_count "
+                      "%d\n", __func__, ramblock, ramblock->base_uva,
+                      ramblock->size, ramblock->ref_count);
+
+            ramblock_ref(ramblock);
             return ramblock;
+        }
     }
 
     hax_warning("can not find 0x%llx in ramblock list.\n", uva);
@@ -362,4 +390,41 @@ hax_chunk * ramblock_get_chunk(hax_ramblock *block, uint64 uva_offset,
     }
 done:
     return block->chunks[chunk_index];
+}
+
+void ramblock_ref(hax_ramblock *block)
+{
+    if (block == NULL) {
+        hax_error("%s: Invalid RAM block\n", __func__);
+        return;
+    }
+
+    ++block->ref_count;
+    hax_debug("%s: block (%p): base_uva = 0x%llx, size = 0x%llx, ref_count = "
+              "%d\n", __func__, block, block->base_uva, block->size,
+              block->ref_count);
+}
+
+void ramblock_deref(hax_ramblock *block)
+{
+    if (block == NULL) {
+        hax_error("%s: Invalid RAM block\n", __func__);
+        return;
+    }
+    if (block->ref_count <= 0) {
+        hax_error("%s: Unreferenced block (ref_count = %d)\n", __func__,
+                  block->ref_count);
+        return;
+    }
+    if (--block->ref_count == 0) {
+        hax_debug("%s: Reset RAM block (%p): base_uva = 0x%llx, size = 0x%llx, "
+                  "ref_count = %d\n", __func__, block, block->base_uva,
+                  block->size, block->ref_count);
+        ramblock_free_chunks(block, false);
+        return;
+    }
+
+    hax_debug("%s: block (%p): base_uva = 0x%llx, size = 0x%llx, ref_count = "
+              "%d\n", __func__, block, block->base_uva, block->size,
+              block->ref_count);
 }