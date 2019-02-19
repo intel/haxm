7e98480c8b98bc196a3a97e6471f988312710cc9
Kamil Rytarowski <n54@gmx.com>
2019-02-19 14:54:41 +0100

    [netbsd] Correct retrieving the CPU index
    curcpu()->ci_cpuid is not guaranteed to return incremental indices 0..N-1.
    
    This caused crashes on some NetBSD machines.
    
    Patch and report by Andreas Gustafsson (NetBSD).
    
    Signed-off-by: Kamil Rytarowski <n54@gmx.com>

--- platforms/netbsd/hax_wrapper.c
+++ platforms/netbsd/hax_wrapper.c
@@ -35,6 +35,7 @@
 #include <sys/mutex.h>
 #include <sys/systm.h>
 #include <sys/xcall.h>
+#include <sys/cpu.h>
 #include <machine/cpu.h>
 #include <machine/cpufunc.h>
 
@@ -61,7 +62,7 @@ int hax_log_level(int level, const char *fmt,  ...)
 
 uint32_t hax_cpuid(void)
 {
-    return curcpu()->ci_cpuid;
+    return cpu_index(curcpu());
 }
 
 typedef struct smp_call_parameter {
