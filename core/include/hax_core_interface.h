50ef1b8607d623cc05e6eaf4025cbe7595668f98
Wenchao Wang <wenchao.wang@intel.com>
2021-04-07 17:24:52 +0800

    cpuid: Add new IOCTL HAX_VCPU_IOCTL_GET_CPUID
    Add a new IOCTL HAX_VCPU_IOCTL_GET_CPUID to enable retrieving CPUID
    features for guest vCPUs. The implementation refers to Intel SDM Vol.
    2A 3.2 CPUID. This version completes Windows platform support.
    
    Signed-off-by: Wenchao Wang <wenchao.wang@intel.com>

--- core/include/hax_core_interface.h
+++ core/include/hax_core_interface.h
@@ -46,6 +46,7 @@ int vcpu_get_fpu(struct vcpu_t *vcpu, struct fx_layout *fl);
 int vcpu_set_regs(struct vcpu_t *vcpu, struct vcpu_state_t *vs);
 int vcpu_get_regs(struct vcpu_t *vcpu, struct vcpu_state_t *vs);
 int vcpu_set_cpuid(struct vcpu_t *vcpu, hax_cpuid *cpuid_info);
+int vcpu_get_cpuid(struct vcpu_t *vcpu, hax_cpuid *cpuid_info);
 void vcpu_debug(struct vcpu_t *vcpu, struct hax_debug_t *debug);
 
 void * get_vcpu_host(struct vcpu_t *vcpu);
